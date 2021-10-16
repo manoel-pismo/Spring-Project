46c74a8f7f7b43feb213ac6b40493864783f668e
Manoel Rachadel Neto <manoelch13@gmail.com>
2021-10-16 13:24:59 -0300

    Alterado dependencia 'h2 database' para scope 'test' Adicionado dependencia 'JUnit' version 4.13.2 Adicionado testes unitários para o Repositorio Adicionado Dockerfile Adicionado docker-compose

--- spring-project/src/main/java/com/rachadel/repository/AccountRepository.java
+++ spring-project/src/main/java/com/rachadel/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.rachadel.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,5 +19,5 @@ public interface AccountRepository extends JpaRepository<Account, Long>{
 	
 	Page<Account> findAll(Pageable pageable);
 	
-	Account findByDocumentNumber(String documentNumber);
+	Optional<Account> findByDocumentNumber(String documentNumber);
 }
